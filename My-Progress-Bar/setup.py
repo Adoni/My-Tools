4a64866250864ad10c05202c1faee04f53c1c6cc
Adoni <adoni1203@gmail.com>
2015-06-06 10:56:30 +0800

    add my progress bar

---
+++ My-Progress-Bar/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='MyProgressBar',
+    version='1.0',
+    description='My Progress Bar',
+    author='Xiaofei Sun',
+    author_email='yiyu9261wsx@yeah.net',
+    url='http://adoni.github.io',
+    py_modules=['my_progress_bar'],
+    )
