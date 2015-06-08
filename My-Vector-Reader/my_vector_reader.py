1e35a2600c125cdcded21ed8b410338b166d19c6
Sun Xiaofei <adoni1203@gmail.com>
2015-06-08 20:47:07 +0800

    add an simple embedding cluster reviewer

--- My-Vector-Reader/my_vector_reader.py
+++ My-Vector-Reader/my_vector_reader.py
@@ -1,28 +1,44 @@
+#coding:utf8
 import numpy
+
+class simple_embedding_cluster_viewer:
+    def __init__(self, file_name, coding_type, as_dict=False):
+        self.vocab, self.vectors=read_vectors(file_name,coding_type,as_dict)
+
+    def get_closest_words(self, word, count=10):
+        if type(word) is str:
+            word=word.decode('utf8')
+        if word not in self.vocab:
+            return []
+        word_vec=self.vectors[self.vocab.index(word)]
+        closest_words=numpy.sum((self.vectors-word_vec)**2,axis=1).argsort()
+        return map(lambda x:self.vocab[x],closest_words[1:count+1])
+
 
 def read_vectors(file_name, coding_type, as_dict=False):
     ##MODE:
     ##DICT: return the vectors as dict
     ##LIST: return two lists
-    words=[]
+    vocab=[]
     vectors=[]
     vector_file=open(file_name)
-    words_count,vector_size=map(lambda x:int(x),vector_file.readline()[:-1].split(' '))
+    vocab_count,vector_size=map(lambda x:int(x),vector_file.readline()[:-1].split(' '))
     for line in vector_file:
         line=line.decode(coding_type)[:-1].split(' ')
         word=line[0]
         vector=map(lambda x:float(x),line[1:])
         if not len(vector)==vector_size:
             continue
-        words.append(word)
+        vocab.append(word)
         vectors.append(vector)
     vectors=numpy.array(vectors)
     if as_dict:
-        return dict(zip(words,vectors))
+        return dict(zip(vocab,vectors))
     else:
-        return words,vectors
+        return vocab,vectors
 
 if __name__=='__main__':
-    words,vectors=read_vectors('/Users/sunxiaofei/2012-1-embedding.data','utf8')
-    for word in words[:100]:
-        print word.encode('utf8')
+    viewer=simple_embedding_cluster_viewer('/Users/sunxiaofei/2012-1-embedding.data','utf8')
+    while 1:
+        word=raw_input('Word: ')
+        print ' '.join(viewer.get_closest_words(word))
